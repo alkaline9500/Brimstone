1cc14d8511acfe994ce717cfdd94b8c7e7a2f43a
Nic Manoogian <zimmoz3@verizon.net>
2013-11-13 09:57:24 -0500

    Added locking for brimstone

--- brimstone.sh
+++ brimstone.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+LOCKFILE=.lockfile.brim
 SRC=$1
 DEST=$2
 DEST_EXT=iPad.mp4
 HANDBRAKE_CLI=HandBrakeCLI
 FLAGS=--preset="iPad"
 
-for FILE in `ls $SRC`
-do
-    filename=$FILE.iPad.mp4
-    echo "$HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT $FLAGS"
-done
+
+if [ -e $LOCKFILE ]
+then
+    echo "Conversion already running"
+else
+    touch $LOCKFILE
+    for FILE in `ls $SRC`
+    do
+        filename=$FILE.iPad.mp4
+        $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT $FLAGS
+    done
+    rm $LOCKFILE
+fi
